28871e3ab684e4ea9539a7811a2dc8d78dc32fa2
Josh Lane <me@joshualane.com>
2012-07-12 11:38:10 -0700

    just show ParsingError message

--- lib/zendesk2/error.rb
+++ lib/zendesk2/error.rb
@@ -4,7 +4,7 @@ class Zendesk2::Error < StandardError
   def initialize(wrapped_exception)
     @wrapped_exception = wrapped_exception
     @message = if wrapped_exception.is_a?(Faraday::Error::ParsingError)
-                wrapped_exception.instance_variable_get(:@wrapped_exception).inspect
+                wrapped_exception.message
               elsif wrapped_exception.is_a?(Faraday::Error::ClientError)
                 wrapped_exception.response.inspect
               else
