37acea577aa33738a793020b6f8940f4f8fc0267
Josh Lane <me@joshualane.com>
2012-09-09 12:11:05 -0400

    model organization#name conflicts
    Closes #3

--- spec/organizations_spec.rb
+++ spec/organizations_spec.rb
@@ -7,17 +7,21 @@ describe "organizations" do
     lambda { {name: Zendesk2.uuid} },
     lambda { {name: Zendesk2.uuid} }
 
-  describe do
+  describe "with an organization" do
     let(:organization) { client.organizations.create(name: Zendesk2.uuid) }
     it "should get #users" do
-      user = client.users.create(email: "#{Zendesk2.uuid}@example.org", name: Zendesk2.uuid, organization_id: organization.id)
+      user = client.users.create(email: "#{Zendesk2.uuid}@example.org", name: Zendesk2.uuid, organization: organization)
       organization.users.should include user
     end
 
     it "should get #tickets" do
-      user = client.users.create(email: "#{Zendesk2.uuid}@example.org", name: Zendesk2.uuid, organization_id: organization.id)
+      user = client.users.create(email: "#{Zendesk2.uuid}@example.org", name: Zendesk2.uuid, organization: organization)
       ticket = client.tickets.create(subject: "#{Zendesk2.uuid}@example.org", description: Zendesk2.uuid, requester: user)
       organization.tickets.should include ticket
+    end
+
+    it "should raise on name conflict" do
+      lambda { client.organizations.create(name: organization.name) }.should raise_exception(Zendesk2::Error)
     end
   end
 end
