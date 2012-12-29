371985aecd07b0a24e9a129e88148bba8444c65b
Josh Lane <me@joshualane.com>
2012-12-28 19:50:14 -0800

    moar doc

--- lib/zendesk2/collection.rb
+++ lib/zendesk2/collection.rb
@@ -1,4 +1,9 @@
+# @abstract adds {#create!} method
 class Zendesk2::Collection < Cistern::Collection
+
+  # Attempt creation of resource and explode if unsuccessful
+  # @raise [Zendesk2::Error] if creation was unsuccessful
+  # @return [Cistern::Model] created model
   def create!(attributes={})
     model = self.new(attributes)
     model.save!
--- lib/zendesk2/model.rb
+++ lib/zendesk2/model.rb
@@ -1,11 +1,19 @@
+# @abstract subclass and implement {#save!}
 class Zendesk2::Model < Cistern::Model
-
   attr_accessor :errors
 
+  # @abstract override in subclass
+  # @raise [Zendesk2::Error] if unsuccessful
+  def save!
+    raise NotImplementError
+  end
+
+  # calls {#save!} and sets {#errors} if unsuccessful and applicable
+  # @return [Zendesk2::Model] self, regardless of success
   def save
     save!
   rescue Zendesk2::Error => e
-    self.errors= e.response[:body]["details"].inject({}){|r,(k,v)| r.merge(k => v.map{|e| e["type"] || e["description"]})} rescue nil
+    self.errors = e.response[:body]["details"].inject({}){|r,(k,v)| r.merge(k => v.map{|e| e["type"] || e["description"]})} rescue nil
     self
   end
 
