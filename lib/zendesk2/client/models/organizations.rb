ac41aadcdc74435fabde3bffab7b30652cc7f511
Jacob Burkhart <jburkhart@engineyard.com>
2013-05-10 11:10:14 -0700

    Implement organizations.find_by_external_id based on http://developer.zendesk.com/documentation/rest_api/organizations.html#search-organizations

--- lib/zendesk2/client/models/organizations.rb
+++ lib/zendesk2/client/models/organizations.rb
@@ -3,6 +3,15 @@ class Zendesk2::Client::Organizations < Zendesk2::Collection
 
   model Zendesk2::Client::Organization
 
+  def find_by_external_id(external_id)
+    body = connection.get_organization_by_external_id(external_id).body
+    if data = body.delete("organizations")
+      collection = self.clone.load(data)
+      collection.merge_attributes(Cistern::Hash.slice(body, "count", "next_page", "previous_page"))
+      collection
+    end
+  end
+
   self.collection_method = :get_organizations
   self.collection_root   = "organizations"
   self.model_method      = :get_organization
