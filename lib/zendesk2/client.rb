d9602f569260a6977257ff974672e1309d7f65cf
Josh Lane <me@joshualane.com>
2015-11-10 09:38:15 -0800

    flatten requires

--- lib/zendesk2/client.rb
+++ lib/zendesk2/client.rb
@@ -1,64 +1,144 @@
-def Kernel.require(file)
-  puts "require '#{file}'"
-  super
-end
-
-def require_resource(resource, options={})
-  plural = options[:plural] || "#{resource}s"
-  except = Array(options[:except])
-  other  = Array(options[:and])
-
-  require "zendesk2/models/#{resource}"
-  require "zendesk2/collections/#{plural}"
-
-  require "zendesk2/requests/create_#{resource}"  unless except.include?(:create)
-  require "zendesk2/requests/destroy_#{resource}" unless except.include?(:destroy)
-  require "zendesk2/requests/get_#{plural}"       unless except.include?(:index)
-  require "zendesk2/requests/get_#{resource}"     unless except.include?(:show)
-  require "zendesk2/requests/update_#{resource}"  unless except.include?(:update)
-
-  other.each { |file| require "zendesk2/requests/#{file}" }
-end
-
 require 'zendesk2/real'
 require 'zendesk2/mock'
 require 'zendesk2/request'
 require 'zendesk2/model'
 require 'zendesk2/collection'
 require 'zendesk2/help_center'
-
+require 'zendesk2/requests/create_help_center_article.rb'
+require 'zendesk2/requests/create_help_center_category.rb'
+require 'zendesk2/requests/create_help_center_section.rb'
+require 'zendesk2/requests/destroy_help_center_article.rb'
+require 'zendesk2/requests/destroy_help_center_category.rb'
+require 'zendesk2/requests/destroy_help_center_section.rb'
+require 'zendesk2/requests/get_help_center_article.rb'
+require 'zendesk2/requests/get_help_center_articles.rb'
+require 'zendesk2/requests/get_help_center_categories.rb'
+require 'zendesk2/requests/get_help_center_categories_articles.rb'
+require 'zendesk2/requests/get_help_center_categories_sections.rb'
+require 'zendesk2/requests/get_help_center_category.rb'
+require 'zendesk2/requests/get_help_center_section.rb'
+require 'zendesk2/requests/get_help_center_sections.rb'
+require 'zendesk2/requests/get_help_center_sections_articles.rb'
+require 'zendesk2/requests/search_help_center_articles.rb'
+require 'zendesk2/requests/update_help_center_article.rb'
+require 'zendesk2/requests/update_help_center_category.rb'
+require 'zendesk2/requests/update_help_center_section.rb'
+require 'zendesk2/models/help_center/article.rb'
+require 'zendesk2/models/help_center/category.rb'
+require 'zendesk2/models/help_center/section.rb'
+require 'zendesk2/collections/help_center/articles.rb'
+require 'zendesk2/collections/help_center/categories.rb'
+require 'zendesk2/collections/help_center/sections.rb'
 require 'zendesk2/requests/search'
 require 'zendesk2/models/audit_event'
-
-require_resource("category", plural: "categories")
-require_resource("forum")
-require_resource("group", and: ["get_assignable_groups"])
-require_resource("user", and: [
-  "search_user",
-  "get_current_user",
-  "mark_user_identity_primary",
-  "get_user_memberships",
-  "get_user_organizations",
-])
-
-require_resource("ticket", and: ["get_requested_tickets", "get_ccd_tickets"])
-require_resource("ticket_audit", except: [:create, :destroy, :update])
-require_resource("ticket_field")
-require_resource("topic")
-require_resource("topic_comment", except: [:update])
-require_resource("ticket_comment", except: [:create, :destroy, :update, :show])
-require_resource("organization", and: [
-  "get_organization_users",
-  "get_organization_tickets",
-  "get_organization_by_external_id",
-  "get_organization_memberships",
-  "search_organization",
-])
-require_resource("user_field")
-require_resource("user_identity", plural: "user_identities")
-require_resource("membership", except: [:update], and: "mark_membership_default")
-require_resource("view", and: ["get_view_tickets"])
-
-def Kernel.require(file)
-  super
-end
+require 'zendesk2/models/category'
+require 'zendesk2/collections/categories'
+require 'zendesk2/requests/create_category'
+require 'zendesk2/requests/destroy_category'
+require 'zendesk2/requests/get_categories'
+require 'zendesk2/requests/get_category'
+require 'zendesk2/requests/update_category'
+require 'zendesk2/models/forum'
+require 'zendesk2/collections/forums'
+require 'zendesk2/requests/create_forum'
+require 'zendesk2/requests/destroy_forum'
+require 'zendesk2/requests/get_forums'
+require 'zendesk2/requests/get_forum'
+require 'zendesk2/requests/update_forum'
+require 'zendesk2/models/group'
+require 'zendesk2/collections/groups'
+require 'zendesk2/requests/create_group'
+require 'zendesk2/requests/destroy_group'
+require 'zendesk2/requests/get_groups'
+require 'zendesk2/requests/get_group'
+require 'zendesk2/requests/update_group'
+require 'zendesk2/requests/get_assignable_groups'
+require 'zendesk2/models/user'
+require 'zendesk2/collections/users'
+require 'zendesk2/requests/create_user'
+require 'zendesk2/requests/destroy_user'
+require 'zendesk2/requests/get_users'
+require 'zendesk2/requests/get_user'
+require 'zendesk2/requests/update_user'
+require 'zendesk2/requests/search_user'
+require 'zendesk2/requests/get_current_user'
+require 'zendesk2/requests/mark_user_identity_primary'
+require 'zendesk2/requests/get_user_memberships'
+require 'zendesk2/requests/get_user_organizations'
+require 'zendesk2/models/ticket'
+require 'zendesk2/collections/tickets'
+require 'zendesk2/requests/create_ticket'
+require 'zendesk2/requests/destroy_ticket'
+require 'zendesk2/requests/get_tickets'
+require 'zendesk2/requests/get_ticket'
+require 'zendesk2/requests/update_ticket'
+require 'zendesk2/requests/get_requested_tickets'
+require 'zendesk2/requests/get_ccd_tickets'
+require 'zendesk2/models/ticket_audit'
+require 'zendesk2/collections/ticket_audits'
+require 'zendesk2/requests/get_ticket_audits'
+require 'zendesk2/requests/get_ticket_audit'
+require 'zendesk2/models/ticket_field'
+require 'zendesk2/collections/ticket_fields'
+require 'zendesk2/requests/create_ticket_field'
+require 'zendesk2/requests/destroy_ticket_field'
+require 'zendesk2/requests/get_ticket_fields'
+require 'zendesk2/requests/get_ticket_field'
+require 'zendesk2/requests/update_ticket_field'
+require 'zendesk2/models/topic'
+require 'zendesk2/collections/topics'
+require 'zendesk2/requests/create_topic'
+require 'zendesk2/requests/destroy_topic'
+require 'zendesk2/requests/get_topics'
+require 'zendesk2/requests/get_topic'
+require 'zendesk2/requests/update_topic'
+require 'zendesk2/models/topic_comment'
+require 'zendesk2/collections/topic_comments'
+require 'zendesk2/requests/create_topic_comment'
+require 'zendesk2/requests/destroy_topic_comment'
+require 'zendesk2/requests/get_topic_comments'
+require 'zendesk2/requests/get_topic_comment'
+require 'zendesk2/models/ticket_comment'
+require 'zendesk2/collections/ticket_comments'
+require 'zendesk2/requests/get_ticket_comments'
+require 'zendesk2/models/organization'
+require 'zendesk2/collections/organizations'
+require 'zendesk2/requests/create_organization'
+require 'zendesk2/requests/destroy_organization'
+require 'zendesk2/requests/get_organizations'
+require 'zendesk2/requests/get_organization'
+require 'zendesk2/requests/update_organization'
+require 'zendesk2/requests/get_organization_users'
+require 'zendesk2/requests/get_organization_tickets'
+require 'zendesk2/requests/get_organization_by_external_id'
+require 'zendesk2/requests/get_organization_memberships'
+require 'zendesk2/requests/search_organization'
+require 'zendesk2/models/user_field'
+require 'zendesk2/collections/user_fields'
+require 'zendesk2/requests/create_user_field'
+require 'zendesk2/requests/destroy_user_field'
+require 'zendesk2/requests/get_user_fields'
+require 'zendesk2/requests/get_user_field'
+require 'zendesk2/requests/update_user_field'
+require 'zendesk2/models/user_identity'
+require 'zendesk2/collections/user_identities'
+require 'zendesk2/requests/create_user_identity'
+require 'zendesk2/requests/destroy_user_identity'
+require 'zendesk2/requests/get_user_identities'
+require 'zendesk2/requests/get_user_identity'
+require 'zendesk2/requests/update_user_identity'
+require 'zendesk2/models/membership'
+require 'zendesk2/collections/memberships'
+require 'zendesk2/requests/create_membership'
+require 'zendesk2/requests/destroy_membership'
+require 'zendesk2/requests/get_memberships'
+require 'zendesk2/requests/get_membership'
+require 'zendesk2/requests/mark_membership_default'
+require 'zendesk2/models/view'
+require 'zendesk2/collections/views'
+require 'zendesk2/requests/create_view'
+require 'zendesk2/requests/destroy_view'
+require 'zendesk2/requests/get_views'
+require 'zendesk2/requests/get_view'
+require 'zendesk2/requests/update_view'
+require 'zendesk2/requests/get_view_tickets'
