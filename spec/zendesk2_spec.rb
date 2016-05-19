cec57e4110c188f0d68104ffb614a03bb5a083b9
Joshua Lane <me@joshualane.com>
2016-05-19 08:52:34 -0700

    deprecate Zendesk2::Client constant

---
+++ spec/zendesk2_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'Zendesk2::Client', 'messages' do
+  around do |example|
+    current = Cistern.deprecation_warnings?
+    Cistern.deprecation_warnings = true
+    example.run
+    Cistern.deprecation_warnings = current
+  end
+
+  specify('are deprecated') {
+    expect { Zendesk2::Client.mocking? }.to output(/deprecated, use Zendesk2/).to_stderr_from_any_process
+  }
+end
