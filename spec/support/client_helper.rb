2b25d5fc6b98bf472f468c56df8516dfac7e109b
Joshua Lane <me@joshualane.com>
2016-05-25 08:32:33 -0700

    remove password from default test client options
    * password auth is favored but sometimes throws 301s in the API

--- spec/support/client_helper.rb
+++ spec/support/client_helper.rb
@@ -5,7 +5,6 @@ module ClientHelper
     options.merge!(logger: Logger.new(STDOUT)) if ENV['VERBOSE']
     options = {
       :username => "zendesk2@example.org",
-      :password => "password",
       :url      => "https://www.zendesk.com",
     }.merge(Zendesk2.defaults).merge(options)
 
