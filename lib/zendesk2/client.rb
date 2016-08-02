6e9730d59effad86405e3d396f38709cb00ada42
Joshua Lane <me@joshualane.com>
2016-08-02 15:10:14 -0700

    style(client): implement respond_to_missing?
    in a way that rubocop does not complain

--- lib/zendesk2/client.rb
+++ lib/zendesk2/client.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 # @deprecated use {#Zendesk2}
 module Zendesk2::Client
-  def self.method_missing(message, *args, &block)
-    Cistern.deprecation('Zendesk2::Client is deprecated, use Zendesk2', caller[1])
-
-    if Zendesk2.respond_to?(message)
-      Zendesk2.public_send(message, *args, &block)
-    else
-      super
+  class << self
+    def respond_to_missing?(method, *)
+      Zendesk2.respond_to?(method) || super
     end
-  end
 
-  def self.respond_to?(message)
-    Cistern.deprecation('Zendesk2::Client is deprecated, use Zendesk2', caller[1])
+    def method_missing(message, *args, &block)
+      Cistern.deprecation('Zendesk2::Client is deprecated, use Zendesk2', caller[1])
 
-    Zendesk2.respond_to?(message) || super
+      if Zendesk2.respond_to?(message)
+        Zendesk2.public_send(message, *args, &block)
+      else
+        super
+      end
+    end
   end
 end
